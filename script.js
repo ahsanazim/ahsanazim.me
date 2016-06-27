f5618d71421955917b4c09aa55d66defcd4cc1b3
AhsanAzim <ahsan-azim@live.com>
2016-06-26 23:18:44 -0400

    button clicking test pt.4

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
   $("#nav_bar").hide().delay(3000).fadeIn();
   $(".main_heading").effect( "pulsate", {times:3}, 3000 );
   $("#nav_home").click(function() {
-    console.log("home button clicked")
+    console.log("#nav_home clicked")
+  });
+  $("#nav_this_website").click(function() {
+    console.log("#nav_this_website clicked")
+  });
+  $("#nav_contact").click(function() {
+    console.log("#nav_contact clicked")
+  });
+  $("#nav_about").click(function() {
+    console.log("#nav_about clicked")
   });
 });
