7992c0590c73882d12855c80ead34913d1d96e31
AhsanAzim <ahsan-azim@live.com>
2016-06-27 19:47:02 -0400

    footer pt.32

--- script.js
+++ script.js
@@ -6,30 +6,32 @@ $(document).ready(function() {
   $("#nav_bar").hide().delay(2500).fadeIn();
   $(".main_heading").effect( "pulsate", {times:2}, 2500);
   $("#nav_home").click(function() {
-    console.log("#nav_home clicked");
     $('html, body').animate({
       scrollTop: $( "#nav_bar" ).offset().top
     }, 500);
     return false;
   });
   $("#nav_this_website").click(function() {
-    console.log("#nav_this_website clicked");
     $('html, body').animate({
       scrollTop: $( "#this_website" ).offset().top - 50
     }, 500);
     return false;
   });
   $("#nav_contact").click(function() {
-    console.log("#nav_contact clicked");
     $('html, body').animate({
       scrollTop: $( "#contact" ).offset().top - 50
     }, 500);
     return false;
   });
   $("#nav_about").click(function() {
-    console.log("#nav_about clicked");
     $('html, body').animate({
       scrollTop: $( "#about" ).offset().top - 50
+    }, 500);
+    return false;
+  });
+  $("#nav_lorem_ipsum").click(function() {
+    $('html, body').animate({
+      scrollTop: $( "#lorem_ipsum" ).offset().top - 50
     }, 500);
     return false;
   });
