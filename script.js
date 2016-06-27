0ccb6989890992c12a80d465932d2728cc440f7d
AhsanAzim <ahsan-azim@live.com>
2016-06-26 23:10:39 -0400

    button clicking test

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 // http://www.tutorialspoint.com/jquery/effect-pulsate.htm
+// https://teamtreehouse.com/community/how-to-make-a-button-and-text-appear-after-a-delay
 $(document).ready(function() {
   $("#nav_bar").hide().delay(3000).fadeIn();
   $(".main_heading").effect( "pulsate", {times:3}, 3000 );
+});
+
+$( "#home" ).click(function() {
+  console.log("home button clicked")
 });
