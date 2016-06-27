978e6be80d178eec7de9153c8fb72873e2852e32
AhsanAzim <ahsan-azim@live.com>
2016-06-26 23:02:09 -0400

    JS plus pulsating text pt.8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // http://www.tutorialspoint.com/jquery/effect-pulsate.htm
 $(document).ready(function() {
-  $("#nav_bar").hide().delay(10000).fadeIn();
+  $("#nav_bar").hide().delay(3000).fadeIn();
   $(".main_heading").effect( "pulsate", {times:3}, 3000 );
 });
