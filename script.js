1155b8da7cc80953217eabd056e45169e706b1de
AhsanAzim <ahsan-azim@live.com>
2016-06-27 18:20:39 -0400

    adding lorem ipsum

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 // http://www.tutorialspoint.com/jquery/effect-pulsate.htm
 // https://teamtreehouse.com/community/how-to-make-a-button-and-text-appear-after-a-delay
+// http://stackoverflow.com/questions/21835012/onclick-function-based-on-element-id
 $(document).ready(function() {
-  $("#nav_bar").hide().delay(3000).fadeIn();
-  $(".main_heading").effect( "pulsate", {times:3}, 3000 );
+  $("#nav_bar").hide().delay(2500).fadeIn();
+  $(".main_heading").effect( "pulsate", {times:2}, 2500);
   $("#nav_home").click(function() {
     console.log("#nav_home clicked")
   });
