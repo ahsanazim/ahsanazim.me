b3db355ea994debcffc137ad770c018a799a5a73
AhsanAzim <ahsan-azim@live.com>
2016-06-27 22:47:01 -0400

    final v 3.0

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
   $("#nav_this_website").click(function() {
     console.log("activated");
     $('html, body').animate({
-      scrollTop: $( "#this_website_proxy" ).offset().top 
+      scrollTop: $( "#this_website_proxy" ).offset().top - 50
     }, 500);
     return false;
   });
